e3d62628f0e116782b0a166fe5ab1be69c1c66d0
keval1903 <74345264+keval1903@users.noreply.github.com>
2026-08-15 22:27:57 +0530

    Delete fix_session_rpc.sql

--- fix_session_rpc.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION public.update_my_session_token(new_token TEXT) 
-RETURNS void 
-LANGUAGE sql 
-SECURITY DEFINER 
-AS $$ 
-  UPDATE user_roles SET current_session_token = new_token WHERE id = auth.uid(); 
-$$;
